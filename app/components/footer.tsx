b565bfb127e25e184ae70eab5ab6c022e3e41e4e
Nico Albanese <nico.albanese@vercel.com>
2025-01-13 10:10:42 +0000

    update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -32,13 +32,13 @@ export default function Footer() {
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
-          <FooterLink href="/rss">rss</FooterLink>
-        </li>
-        <li>
           <FooterLink href="https://github.com/nicoalbanese">github</FooterLink>
         </li>
         <li>
           <FooterLink href="https://x.com/nicoalbanese10">twitter</FooterLink>
+        </li>
+        <li>
+          <FooterLink href="https://www.youtube.com/@nicoalbanese10">youtube</FooterLink>
         </li>
       </ul>
       {/* <p className="mt-8 text-neutral-600 dark:text-neutral-300">
