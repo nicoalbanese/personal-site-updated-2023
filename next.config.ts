5e6094f9b0ed8ae611959bca27f80cac9adcdf0d
Nico Albanese <nico.albanese@vercel.com>
2025-01-26 19:34:31 +0000

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["next-mdx-remote"],
+  async redirects() {
+    return [
+      {
+        source: "/writing/vc-os",
+        destination: "/blog/vc-os",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
